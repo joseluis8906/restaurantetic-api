3303597bce9b326dc3da4d88181b2d999e3cfecf
Jose Cáceres <joseluis8906@opmbx.org>
2018-08-12 22:05:23 -0500

    Actualizado

---
+++ src/test/java/com/restaurantic/registro/RegistroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restaurantic.registro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
