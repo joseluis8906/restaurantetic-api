9e6d39a0b19e0231d465fdb5cf57b05bb6d05da1
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-06-11 21:52:53 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	deleted:    run_almundo_api.sh
    	modified:   run_restaurantetic_api.sh

--- run_almundo_api.sh
+++
@@ -1,6 +0,0 @@
-docker run -it\
-	--name almundo-api\
-	-p 3000:3000\
-	-p 9229:9229\
-	-v $HOME/Developments/almundo-api:/app\
-	-d node:10.15.3-stretch
--- run_restaurantetic_api.sh
+++ run_restaurantetic_api.sh
@@ -1,5 +1,5 @@
 docker run -it\
 	--name restaurantetic-api\
 	-p 9999:8080\
-	-v $HOME/Developments/restaurantetic-api:/app\
+	-v $(pwd):/app\
 	-d joseluis8906/openjdk:8u181-jdk-stretch
